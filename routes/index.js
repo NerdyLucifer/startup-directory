144ddece1e7db10084d9413d7981ba449618aad5
NerdyLucifer <ashwat.kumarsingh.met20@itbhu.ac.in>
2022-02-10 14:09:12 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ router.get("/startups", async (request, response) => {
   console.log("Hello");
   await dbConnect();
   try {
-    let finder = await Startup.find().lean();
+    let finder = await Startup.find().sort({'Name':1});
+    console.log(finder)
     response.status(201).send(finder);
   } catch (error) {
     response.send("Some error happened");
